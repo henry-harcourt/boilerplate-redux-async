a2f71f52d0ebb8568b2b664ddf963d32409877fc
student <student@eda-bc-04.localdomain>
2020-02-12 11:40:31 +1300

    added onSubmit

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,13 +2,55 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({children, dispatch}) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+
+class LoadSubreddit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      source: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.dispatch(fetchPosts(this.state.subreddit))
+  
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Search for SubReddit</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            name='source'
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </form>
+
+        <button onClick={() => this.props.dispatch(fetchPosts('newzealand'))}>
+          Fetch Posts
+        </button>
+        {/* {children} */}
+      </div>
+    )
+  }
+}
+
+
+
 
 export default connect()(LoadSubreddit)
